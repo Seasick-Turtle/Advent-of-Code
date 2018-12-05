ffbb96c112fb7a5f046e23356ac527180cb1bba8
Christopher Martin <martincdev@gmail.com>
2018-12-05 04:29:07 -0600

    add day-2 part 1 solution

---
+++ day-2/part1.js
@@ -0,0 +1,99 @@
+const data = [ 
+  'xdmgyjkpruszabaqwficevtjeo', 'xdmgybkgwuszlbaqwfichvtneo', 'xdmgyjkpruszlbcwwfichvtndo', 'xdmgcjkprusyibaqwfichvtneo', 'xdmgyjktruszlbwqwficuvtneo', 'xdmgxjkpruszlbaqyfichvtnvo', 
+  'xdmgytkpruszlbaqwficuvtnlo', 'xdmgydkpruszlbaqwfijhvtnjo', 'xfmgyjkmruszlbaqwfichvtnes', 'xdmgyrktruszlraqwfichvtneo', 'xdmgyjkihuszlbaqdfichvtneo', 'hdmgyjkpruszeiaqwfichvtneo',
+  'xdmzyjkpruszlbaqwgichvtnxo', 'xdmgyjknquszlbpqwfichvtneo', 'idmgyjrpruszlbtqwfichvtneo', 'xkmgyjkpruuzlbaqwfichvfneo', 'xdmgyjkpruszlfaqwficnvtner', 'xdmgyjkpruszlbpqwficwvteeo', 
+  'xdmgyjkpwuszlbiqwfhchvtneo', 'xdmgyjkpruszwbaqwfichrtnbo', 'xdpgyjkprusblbaqwfgchvtneo', 'xdmryjkcruszlbaqwfichvtnee', 'xwmgylkpruszlbaqwfcchvtneo', 'xdmgyjkpruszflaqwfixhvtneo',
+  'xdmgyjkmruszloaqwfichvteeo', 'xvmgrjkpruszlbaqwfichvsneo', 'xdmvyjkprusmlbaqwfichvtnes', 'xdmgyjkpruszlbaqwfichkgbeo', 'xdmgyikpruxzlbaqwfichvtnei', 'xdmgyjkprugzlbaqhfichvtveo', 
+  'xdmgyjkpruszlbaqjaichftneo', 'xdmzijkpruszlbaqwwichvtneo', 'xdmgyjkprsszlbaqwfihhvlneo', 'xdmgyjkprusqlwaqzfichvtneo', 'ximgyjkpruszlbawwfichvtnen', 'xsmgyjzpruszlbaqwfichvaneo', 
+  'xdmgyjkpruszlcaoyfichvtneo', 'xdmgyjkprusmlbaqvnichvtneo', 'xdmgyjkvruszmbaqwfichvtueo', 'xdmgyjppuuszleaqwfichvtneo', 'xddgyjkprubzlbaqwfichvaneo', 'xdmgwjkpruszebaswfichvtneo',
+  'xdogyjkpruszlblqwfichvdneo', 'xdkgyjgpruszlbaqwfizhvtneo', 'xdvgyjkpruszlbdqwfichvtqeo', 'xdmgyjlpruszlbapwficgvtneo', 'xdmgyjkpruszlbaqofickvtngo', 'xdmgyjkprqszliaywfichvtneo', 
+  'xdqgyjkpruszlbcqwficnvtneo', 'xdmgdjkpruszlbaqwxichvtseo', 'xdmgyjkpruczlbaqwfichdtnfo', 'xdmgyjkpruszluaqwficzvtnjo', 'xdmgyjkproszlbaqwfacevtneo', 'xfmgijkpruszlbrqwfichvtneo', 
+  'odmgyjkpluszlbaqwfichvuneo', 'xdmgyjkpruszlbaqwwichukneo', 'xdmgdjkpruszwbaqwfichvtnet', 'xdmgyjkzrusvlbaqwrichvtneo', 'xdmgylkprutzlbaqwfichvtnbo', 'xdmgyjkpruszsbaqwfijtvtneo', 
+  'xdmgyjkproszlbjqwfichntneo', 'xdmgyhkpluszlbaqwfichvtnlo', 'xdmgyjhprushlbaqwfichvtnzo', 'gdmoyjkpruszlbarwfichvtneo', 'cdmgyjkpruszlbaqwfcchvtned', 'xgmgyjkpruszlbaqwfschvtnek', 
+  'xdmgyjkprusnlzamwfichvtneo', 'xdmgyjkprgszlbaxwfichvuneo', 'txmgyjksruszlbaqwfichvtneo', 'xdmgyjkprusbbbpqwfichvtneo', 'xdmoyjkpruszlbaqwfighvtxeo', 'xdmgyjkpruslhbaqwfichptneo', 
+  'xdmgzjkpruszlbaqwffcmvtneo', 'xdmgyjkiruszlbaqgficuvtneo', 'vdbgyjkpruszlbaqwfichvtnek', 'xdmgyjspruszlbaqwfochvtney', 'xdmgyjkpruszibaqwfivhvteeo', 'xdmgyjkpruszfbaqwficbvtgeo', 
+  'xdmgyjkprystlbaqwxichvtneo', 'xdmfyjkpryszlxaqwfichvtneo', 'xdmgyjgpruspybaqwfichvtneo', 'xdmgyjklruszlbjqwdichvtneo', 'xdmgyjkzruszltaqwfichvtnek', 'xdmgqjkpruszlzaqwfichvtneh', 
+  'xdmgyjhnruszlbaqwficqvtneo', 'xdmgyjkproszlbaqweichvtnez', 'xdmgyjkprurzlbaawfichytneo', 'xdmgyfkpruszlbaqwfschutneo', 'xdmnyjkpruszlbaawjichvtneo', 'xdmgyjkpybszlbaqwfichvwneo', 
+  'xdmgtjkhruszlbaqwfichatneo', 'xamgyjkprurzlbaqwfichvaneo', 'xdmgyjkpruszlbaqwgichvtnqv', 'ndmgyjkpruszlsaqwfuchvtneo', 'xdmgygkpgusrlbaqwfichvtneo', 'xdmgyjkpruszfbaqwfichvtnmy', 
+  'xdmgyjkprupflbaqwfichvjneo', 'ndmgyjkpruszlbagwfichvtnxo', 'xdmgyjkpruszlbafwfilhvcneo', 'xdmgyjkpruszlbaqwfichvjsea', 'xebgyjkpruszlbaqafichvtneo', 'xdmkyjdpruszlbaqwfichvtnei', 
+  'xomgyjkprufzlbaqwfochvtneo', 'xdmgyjkprfsllbaqwfiihvtneo', 'xdmyyjkpruszebaqwficmvtneo', 'xdmnyjkpruczlbarwfichvtneo', 'xdmgyjkpruszcbaqwbichvtneg', 'xdmgxjkpluszlbapwfichvtneo', 
+  'xgrlyjkpruszlbaqwfichvtneo', 'xdmgyjkpruszlraqwxcchvtneo', 'xdmhyjupruszlbaqafichvtneo', 'xdmgnjkpruszlbkqwfjchvtneo', 'xdmgyjkpruszlwaqwfichvtndg', 'xdmgfjkpruvqlbaqwfichvtneo', 
+  'xdmgejkptuszlbdqwfichvtneo', 'xlmgyjkpruszlnaqwfochvtneo', 'xdmgcjkpruszlbaqwfiqhvaneo', 'xdmgyjupruyzlbaywfichvtneo', 'gdmgyjkpruyzlbaqwficevtneo', 'xdmgyjkaruazlbapwfichvtneo', 
+  'xsmiyjkpruszlbaqwfichvtveo', 'xdmiyjkprukzlbaqwfichvtnea', 'xdbgmjkxruszlbaqwfichvtneo', 'xdmgyjkpruskvbaqwfichdtneo', 'xdmgyjkprusznbaqwficshtneo', 'xdmgyjkprusrlbaqwfzchetneo', 
+  'xdmgyrkpruszzbaqwfichvtned', 'xdmgyjkprusolbacwmichvtneo', 'xdmgypkpruszlbaqwfichvtmgo', 'xdmgyjkprumzlbhqwfichttneo', 'xdmgydkprusglbaqwfichvtnei', 'xdmuyjkpruszlbpqwfichvtyeo', 
+  'xdmtymkprusslbaqwfichvtneo', 'xdmgyjjprkszlbaqwfqchvtneo', 'xdmgvjdpruszlbaqwfichgtneo', 'xdtgyjkpruwzlbaqwfjchvtneo', 'xdmgyjkpruszlbafseichvtneo', 'xdmgvjkpruszlraawfichvtneo', 
+  'xdmgyukprgszlbatwfichvtneo', 'xhmgyjkpruszliaqwnichvtneo', 'xdmgyjspruszlbwqyfichvtneo', 'xdmgyjkjruszlqaqwfichvtnvo', 'xdmgyjkiruszlbnqwfichmtneo', 'ximgyjkpruszlbaqwfvcevtneo', 
+  'xdmdyjkpruszlbaqwsithvtneo', 'ndmgyjkpruszlbaqwfilhatneo', 'xdmgyjkpruszlbaqwfinhvcnez', 'xdmgypkpsuszlbajwfichvtneo', 'xdpgmjkpluszlbaqwfichvtneo', 'xdmgyjnprupzlbaqwfichvtnel', 
+  'xbmgyjkprmszlfaqwfichvtneo', 'xdmgyjkpausllbaqwfichvtseo', 'xdmgyjkpruszlbaqwfqchttnes', 'xgmgyjkpruszlbaxwfichvtneb', 'xdmgyjkpruszabqqwfichvineo', 'xdmgpjkpquszlbaqwfichvdneo', 
+  'xdmgyjkeruszlbaqdficbvtneo', 'xdmaujkpruszlbaqwfichvteeo', 'xdmgyjkpruszlbaqwrirhvtnev', 'xdmgyjkpsugzllaqwfichvtneo', 'xdmgyjkpruszlbaqwfichctnlm', 'xdmeyjkpruszlbacwfiwhvtneo', 
+  'xdmgyjkpiuhzlbaqwfijhvtneo', 'xdmgyjkpruszlbmqhfiohvtneo', 'xdegyjkpbuszlbbqwfichvtneo', 'xdmggxkpruszlbaqwfirhvtneo', 'xdmgojkpruszlbaqvfichvtteo', 'xdmgyjhtruszlbaqwmichvtneo', 
+  'rdmgyjkpruszlbaqwfichvthek', 'xdlgyjqpruszlbaqwfbchvtneo', 'xdmgyjspriszlbavwfichvtneo', 'rdkgyjkpruszlbaqwfichvtnuo', 'tdmgyjkuruszlbaqwfichvtnev', 'xdmgyjkpxuszlbaqwfkchvtnso', 
+  'xdegyjkpruszlbbqxfichvtneo', 'xdmgyjkpruszlbaqwficpvtket', 'xdmgyjkpruszliaqwfnchvtnec', 'xdmgyjkpreszlbaqwficdvtdeo', 'rdmgyjkpruszlbaywfychvtneo', 'xdmgywkpruszlbaqwficrvtaeo', 
+  'xdmgyjkpruszlbanwflchvoneo', 'xdmgyjkpruyzlbaqufychvtneo', 'symgyjkpruszlbaqwfichvtqeo', 'xdmgyjkpruszlbaqwfichvbzqo', 'xzfgyjkpruszlbaqwfichvtveo', 'udmgyjepruszlbaqwfichbtneo', 
+  'xhmgyjkpruszlbaqwfjchvtnef', 'xdhgyjkpruszlbaqaftchvtneo', 'xdmzyjkjruszlbaqwfichvtnwo', 'xdmgyjepruszlbaqwffchvtnef', 'xdmgyjkprurzlbaqwfikhvtneq', 'xomoyjkpruszkbaqwfichvtneo', 
+  'xdmgyjkpiuszubaqwfichktneo', 'xdmgyjkprusdlbaqwhihhvtneo', 'xdmgyjkpruszlbaqwwirhvxneo', 'xdmgyjkpruszlbaqwficgitzeo', 'xdmgyjlpruszlbaqwfichpjneo', 'xjmgyjkpxuszlbaqwfichatneo', 
+  'xdmgylkpruszlbaqwfiehvtnez', 'xdmgbjkpruszmbaqwfihhvtneo', 'xdmgyjkprubzlwaqwfichvtxeo', 'xdmgyjhlrustlbaqwfichvtneo', 'xdmmyjkpruszlbaqwfdchitneo', 'xdmgyjkpruszlbaqwoichhtbeo', 
+  'xdzgyjkprvszlcaqwfichvtneo', 'ndmgyjkpruszlbaqwficavxneo', 'xdmgyjfpruszxbaqzfichvtneo', 'xdmgyjkpeuszlbaqzficdvtneo', 'xdmgyjkpruszlbmqffidhvtneo', 'xdnvyjkpruszlbafwfichvtneo', 
+  'xdygyjkpruszlbljwfichvtneo', 'xdigyjkpruszlbeqwfuchvtneo', 'xdmgyjkpruszlbpzwfichvteeo', 'bdmgyjbpruszldaqwfichvtneo', 'xdmgyjkprrszlbaqmpichvtneo', 'idmgyjkpruszlbaqyfichvtkeo', 
+  'xdmgyjkmrqsclbaqwfichvtneo', 'xdmgyjkpruazlbeqwfichvtxeo', 'ddmgyjkpruszlbsqwfichotneo', 'xdmgyqkpruszjbaqwfxchvtneo', 'xdmnyjkpruozlbaqwfichvtreo', 'edmgyjkpruszlbuqwwichvtneo', 
+  'xdmgyjkprmshlbaqwfichctneo', 'xdmgyjkpruszlbaqwffghotneo', 'xdmcyjkprfszlbaqnfichvtneo', 'xdmgyjypruszhbaqwficyvtneo', 'xdmgyjkprzszlyaqwficmvtneo', 'xlmgyjkprzszlbaqwficyvtneo', 
+  'xdmgyjkprutulbaqwfithvtneo', 'xdygyjkpruszlbpqwfichvpneo', 'xdmgsjkpoumzlbaqwfichvtneo', 'xdmgyjkpyuszlbaqdfnchvtneo', 'xdxgyjkpruszlbaqwfizhvtnjo', 'xdmgyjkpruszlbaqwfschvkndo', 
+  'xdmgpjkprnszlcaqwfichvtneo', 'xhmgyjkpruszlbaqwficgvtnet', 'xdmgyjkpruswlbaqwfichvtqer', 'ddmgyjkprcszlbaqwfichqtneo', 'xdmgyjkpruhhlbaqwpichvtneo', 'xdmgyjkeraszlbaqwfichvtnso', 
+  'nomgyjkpruszlbaqwficavxneo', 'xdmgyjkprdszlbaqwfobhvtneo', 'xdmgyjkprgszlbaqwfichvtdao', 'xomgyjspruswlbaqwfichvtneo', 'xdzgyjkpruszlbaqwficwvpneo', 'admgejkpruszlbaqwfimhvtneo', 
+  'xdtgyjkpruszlmaqwfiqhvtneo', 'xdmgymkprusqlbaqwtichvtneo', 'xdmgyjkpluszlbaqwfidhvtnea', 'ztmgyjjpruszlbaqwfichvtneo'
+]
+
+/*
+For example, if you see the following box IDs:
+
+    abcdef contains no letters that appear exactly two or three times.
+    bababc contains two a and three b, so it counts for both.
+    abbcde contains two b, but no letter appears exactly three times.
+    abcccd contains three c, but no letter appears exactly two times.
+    aabcdd contains two a and two d, but it only counts once.
+    abcdee contains two e.
+    ababab contains three a and three b, but it only counts once.
+
+Of these box IDs, four of them contain a letter which appears exactly twice, 
+and three of them contain a letter which appears exactly three times. Multiplying these 
+together produces a checksum of 4 * 3 = 12.
+*/
+
+const checksumIDs = data => {
+  let doubleIDs = 0;
+  let tripleIDs = 0;
+  let comparison = {};
+
+  for (let i = 0; i < data.length; i++) {
+    comparison[i] = getChar(data[i]);
+
+    // check to see if IDs have 2 of any letter
+    // check to see if these IDs have exactly 3 of any letter or both
+    if (Object.values(comparison[i]).includes(3) && Object.values(comparison[i]).includes(2)) {
+      tripleIDs++;
+      doubleIDs++;
+    } else if (Object.values(comparison[i]).includes(3)) {
+      tripleIDs++;
+    } else if (Object.values(comparison[i]).includes(2)) {
+      doubleIDs++;
+    }
+
+  }
+
+  console.log(doubleIDs);
+  console.log(tripleIDs);
+  // multiply both values to get checksum
+  console.log(doubleIDs * tripleIDs);
+}
+
+const getChar = str => {
+  let charList = {};
+
+  for (let char of str) {
+    charList[char] = charList[char] + 1 || 1;
+  }
+
+  return charList;
+}
+
+checksumIDs(data);
